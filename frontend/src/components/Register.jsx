c295ab937f4eba1cdc5b3e243f02df3d4e5135db
Molnár Péter Levente <Molnar.Peter@MNKH.hu>
2024-06-27 16:36:24 +0200

    .

---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../styles/register.css';
+
+function Register(){
+    return <div>
+        <div className="title">Register</div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate sunt provident quis possimus eveniet repudiandae minus ipsam, quos consequatur sint cumque magnam a eos deserunt nobis labore quisquam, ipsa deleniti.</p>
+    </div>
+}
+
+export default Register
